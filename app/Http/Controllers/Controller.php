e436462547dc58c5096cd0040fc25455fe5057e1
Socha <mattsocha11@gmail.com>
2019-10-24 00:08:56 -0400

    make external API call, and read json from json file

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -8,6 +8,7 @@ use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
 use App\Cities;
+use App\Services;
 
 class Controller extends BaseController
 {
@@ -16,4 +17,17 @@ class Controller extends BaseController
      function getCities() {
        return response()->json(["status" => 0, "cities" => Cities::with(['Services'])->get()], 200);
     }
+
+    function getServiceData($serviceID) {
+      $service = Services::where('id', $serviceID)->first();
+      $path = storage_path() . "/json/" . $service->name . ".json";
+      $json = json_decode(file_get_contents($path), true);
+
+      $client = new \GuzzleHttp\Client();
+
+      $request = $client->request('GET', 'https://thereportoftheweek-api.herokuapp.com/reports');
+      $data = json_decode($request->getBody()->getContents());
+
+      return response()->json(["status" => 0, "data" => $data, "json" => $json], 200);
+   }
 }
