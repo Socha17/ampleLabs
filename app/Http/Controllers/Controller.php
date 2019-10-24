e99f549f75b4bcecc0ac093ba601cc9fc6517e7b
Socha <mattsocha11@gmail.com>
2019-10-24 17:11:51 -0400

    get data from csv file

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,6 +6,7 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use OzdemirBurak\JsonCsv\File\Csv;
 
 use App\Cities;
 use App\Services;
@@ -23,11 +24,14 @@ class Controller extends BaseController
       $path = storage_path() . "/json/" . $service->name . ".json";
       $json = json_decode(file_get_contents($path), true);
 
-      $client = new \GuzzleHttp\Client();
+      $csv = new Csv(storage_path() . '/csv/addresses.csv');
+      $csv->setConversionKey('options', JSON_PRETTY_PRINT|JSON_UNESCAPED_UNICODE|JSON_UNESCAPED_SLASHES);
+      $csvToJson = json_decode($csv->convert());
 
-      $request = $client->request('GET', 'https://thereportoftheweek-api.herokuapp.com/reports');
+      $client = new \GuzzleHttp\Client();
+      $request = $client->request('GET', 'https://thereportoftheweek-api.herokuapp.com/reports?category=Energy Crisis');
       $data = json_decode($request->getBody()->getContents());
 
-      return response()->json(["status" => 0, "data" => $data, "json" => $json], 200);
+      return response()->json(["status" => 0, "data" => $data, "json" => $json, 'contactsForHelp' => $csvToJson], 200);
    }
 }
